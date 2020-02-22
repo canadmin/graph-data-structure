17df673c84519d250179ae0fba43c37e8eaef78d
canadmin <icanyard@gmail.com>
2020-02-22 07:59:41 +0300

    global state

--- src/store/modules/node.js
+++ src/store/modules/node.js
@@ -1,19 +1,45 @@
 const state = {
-  nodeFrom : [],
-  toNode : [],
-  validity : []
+  data : [],
+  activeNodes : [],
+  nodeW : []
 };
 
 const getters = {
-
+  getNodes(state){
+    return state.data;
+  },
+  getActiveNodes(state){
+    return state.activeNodes;
+  },
+  getW(state){
+    return state.nodeW;
+  }
 };
 
 const mutations = {
-
+  setNewGraphData(state,data){
+    console.log("Req Data",data);
+    state.data = data;
+  },
+  newActiveNodes(state,activeNodes){
+    state.activeNodes = activeNodes;
+    console.log("active nodes",activeNodes)
+  },
+  changeNewW(state,newW){
+    state.nodeW = newW;
+  }
 };
 
 const actions = {
-
+    reqNode({commit},data){
+      commit('setNewGraphData',data);
+    },
+    reqActiveNode({commit},activeNodes){
+      commit('newActiveNodes',activeNodes);
+    },
+    setW({commit},newW){
+      commit('changeNewW',newW);
+    }
 };
 
 export default {
@@ -22,3 +48,36 @@ export default {
   mutations,
   actions
 };
+/*
+                //1
+                ctx.beginPath();
+                ctx.arc(350, 200, 30, 0, 2 * Math.PI);
+                ctx.stroke();
+
+
+                //2
+                ctx.beginPath();
+                ctx.arc(550, 100, 30, 0, 2 * Math.PI);
+                ctx.stroke();
+
+
+                //3
+                ctx.beginPath();
+                ctx.arc(750, 200, 30, 0, 2 * Math.PI);
+                ctx.stroke();
+
+                //4
+                ctx.beginPath();
+                ctx.arc(750, 300, 30, 0, 2 * Math.PI);
+                ctx.stroke();
+
+                //5
+                ctx.beginPath();
+                ctx.arc(550, 400, 30, 0, 2 * Math.PI);
+                ctx.stroke();
+
+                //6
+                ctx.beginPath();
+                ctx.arc(350, 300, 30, 0, 2 * Math.PI);
+                ctx.stroke();
+* */
